4c1f682bb76218ac733a55d3ba749baac4a0feb9
BorbaBruno <brunodeborba@outlook.com>
2023-08-27 17:26:36 -0300

    iteraveis

--- aula48_listas.py
+++ aula48_listas.py
@@ -83,8 +83,20 @@ Métodos úteis:
 Create Read Update   Delete
 Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
-lista_a = [1, 2, 3]
-lista_b = [4, 5, 6]
-lista_c = lista_a + lista_b
-lista_a.extend(lista_b)
+#lista_a = [1, 2, 3]
+#lista_b = [4, 5, 6]
+#lista_c = lista_a + lista_b
+#lista_a.extend(lista_b)
+#print(lista_a)
+
+"""
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
+"""
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
 print(lista_a)
+print(lista_b)
