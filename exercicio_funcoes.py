90bb15b23bb3b5c6e94e9f9961bbe62f88f5a919
BorbaBruno <brunodeborba@outlook.com>
2024-01-13 18:16:38 -0300

    exercicio

--- exercicio_funcoes.py
+++ exercicio_funcoes.py
@@ -10,6 +10,17 @@ def multiplica(a, b, c, d):
 resultado = multiplica(1, 2, 3, 4)
 print(resultado)
 
+## OU
+
+def multiplicar(*args):
+    total = 1
+    for numero in args:
+        total*=numero
+        return total
+
+mult = multiplicar(1, 2, 3, 4, 5)
+print(mult)
+
 
 # Crie uma função fala se um número é par ou ímpar.
 # Retorne se o número é par ou ímpar.
